e7c61623880de0bb13ed26f6deea3ab0f6b1b67b
Alex Miller <apmiller108@yahoo.com>
2016-10-14 21:49:15 -0400

    Test password and email validation

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -25,4 +25,26 @@ defmodule Founders.UserTest do
 
     refute changeset.valid?
   end
+
+  test "changeset with a bad email address" do
+    replace_email = fn current_value ->
+      {current_value, "bad email" }
+    end
+    invalid_attrs = Map.get_and_update(@valid_attributes, :email, replace_email)
+
+    changeset = User.changeset %User{}, elem(invalid_attrs, 1)
+
+    refute changeset.valid?
+  end
+
+  test "changeset with a password under 6 characters" do
+    replace_password = fn current_value ->
+      {current_value, "badpw" }
+    end
+    invalid_attrs = Map.get_and_update(@valid_attributes, :email, replace_password)
+
+    changeset = User.changeset %User{}, elem(invalid_attrs, 1)
+
+    refute changeset.valid?
+  end
 end
