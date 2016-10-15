e01b5110054a9246596083bc52e2e0a3b7d9bd9d
David Shvimer <dshvimer@gmail.com>
2016-10-14 20:01:18 -0400

    Helper method to read json for test

--- test/lib/facebook_data_test.exs
+++ test/lib/facebook_data_test.exs
@@ -3,16 +3,22 @@ defmodule Founders.FacebookDataTest do
   doctest Founders.FacebookData
 
   test "Filter by project name" do
-    file = File.cwd! 
-      |> Path.join("facebook_response.json") 
-      |> File.read!
-
-    %{"data" => json} = Poison.decode! ~s({"data": #{file}}), as: %{"data" => [%Founders.FacebookData{}]}
-
-    filtered = Founders.FacebookData.filter_by_project_name(json, "Rentlit")
+    filtered = Founders.FacebookData.filter_by_project_name(read_json, "Rentlit")
     ad = Enum.at(filtered, 0)
 
     assert 1 == Enum.count filtered
     assert "Rentlit" == ad.ad_name
+  end
+
+
+  """
+  Helper method to read in json
+  """
+  defp read_json do
+    file = File.cwd! 
+      |> Path.join("facebook_response.json") 
+      |> File.read!
+    %{"data" => json} = Poison.decode! ~s({"data": #{file}}), as: %{"data" => [%Founders.FacebookData{}]}
+    json
   end
 end
