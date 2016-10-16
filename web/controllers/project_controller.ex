876f1605325a4693a5d0a8c6ab691709015d9773
Dominique Miller <dominique.p.miller@gmail.com>
2016-10-16 10:27:13 -0400

    Ran migration for project model.

--- web/controllers/project_controller.ex
+++ web/controllers/project_controller.ex
@@ -2,19 +2,18 @@ defmodule Founders.ProjectController do
   use Founders.Web, :controller
 
    def create(conn, %{ "project" => project_params }) do
+    changeset = Project.changeset(%Project{}, project_params)
 
-   changeset = Project.changeset(%Project{}, project_params)
-
-   case Repo.insert(changeset) do
-      {:ok, post} ->
-        conn
-        |> put_status(:created)
-        |> put_resp_header("location", projec_path(conn, :show, project))
-        |> render("show.json", project: project)
-      {:error, changeset} ->
-        conn
-        |> put_status(:unprocessable_entity)
-        |> render(RestApi.ChangesetView, "error.json", changeset: changeset)
-    end      
+       case Repo.insert(changeset) do
+          {:ok, post} ->
+            conn
+            |> put_status(:created)
+            |> put_resp_header("location", projec_path(conn, :show, project))
+            |> render("show.json", project: project)
+          {:error, changeset} ->
+            conn
+            |> put_status(:unprocessable_entity)
+            |> render(RestApi.ChangesetView, "error.json", changeset: changeset)
+       end      
   end
 end
