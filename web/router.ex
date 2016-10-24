86b3a67ecfc8eecef9e2e48e4ea31fd34a7372e2
David Shvimer <dshvimer@gmail.com>
2016-10-24 09:15:48 -0400

    Fix merge conflicts

--- web/router.ex
+++ web/router.ex
@@ -9,14 +9,10 @@ defmodule Founders.Router do
     pipe_through :api
     resources "/users", UserController, only: [:create]
   end
-<<<<<<< HEAD
-=======
-
   # Other scopes may use custom stacks.
-   scope "/api", Founders do
-     pipe_through :api
+  scope "/api", Founders do
+    pipe_through :api
 
-     resources "/project", ProjectController, only: [:show, :create]
-   end
->>>>>>> 8e9f86c53e89497a5be23720ddfc934079042b4d
+    resources "/project", ProjectController, only: [:show, :create]
+  end
 end
